fe002db3c1f52a05270828d6fc0c46cd1cbb7212
VL-in <vanessa.hslin@gmail.com>
2025-06-08 13:30:21 -0300

    Criado funções de ordernar a lista biblioteca_virtual (ordernar_lista_biblioteca); funções de adicionar: chave-valor novo na lista (adicionar_doc_na_lista). Modificado a função (listar_doc_dir_org), que lista os documentos do diretório via input e; modificado função (listar_dir).

--- biblioteca.py
+++ biblioteca.py
@@ -3,6 +3,7 @@
 #Importando bibliotecas
 import os
 import shutil
+import os.path
 
 #Criando a biblioteca com os títulos e informações
 Biblioteca_virtual = {
@@ -20,7 +21,7 @@ Biblioteca_virtual = {
     },
     'Iniciando em bioinformática': {
         'data_publicacao': 2019,
-        'tipo': 'ePUB'
+        'tipo': '.ePUB'
     },
     'Dataset - Relatório epidemiológico de dengue em 2024':{
         'data_publicacao': 2025,
@@ -38,11 +39,62 @@ Biblioteca_virtual = {
 
 #Definindo funções
 
-#Listar documentos
-def listar_doc_dir(diretorio):
-    '''Este código lista todos os documentos eletrônicos disponíveis no diretório da biblioteca de forma organizado.'''
+#Listando a biblioteca virtual (OK)
+#Solicitando o tipo de ordenação
+def ordernar_lista_biblioteca():
+    '''Ordena o dicionário "Biblioteca_virtual" por 3 formas: por título (1), por data de publicação (2) e por tipo de documento (3). 
+    Esta função não altera o dicionário global.'''
+    #Trazendo o dicionário existente para dentro da função
+    global Biblioteca_virtual
+    #Solicita o usuário escolher a forma como quer ordernar a lista
+    ordernar_por = input(f'Digite "1" para ordernar por título. \nDigite "2" para ordernar por data de publicação\nDigite "3" para ordernar por tipo de arquivo.\nDigite a opção desejada: ')
+    if ordernar_por == '1':
+        for titulo, valor in sorted(Biblioteca_virtual.items()):
+            print(titulo, valor)
+    elif ordernar_por == '2':
+        for titulo, valor in sorted(Biblioteca_virtual.items(), key=lambda item: item[1]['data_publicacao']):
+            print(titulo,valor)
+    elif ordernar_por == '3':
+        for titulo, valor in sorted(Biblioteca_virtual.items(), key=lambda item: item[1]['tipo']):
+            print(titulo,valor)
+    else:
+        print('Entrada invalida.')
+
+    
+# listar documentos do diretorio (OK)
+def listar_dir():
+    ''' Esta função lista somente os documentos do diretório fornecido pelo usuário e ignora as pastas dentro do diretório.
+        Ela retorna uma lista com os arquivos existentes no diretório.'''
+    #Pedindo para o usuário passar o diretório via teclado
+    diretorio = input(r'Forneça o diretório em que deseja listar os documentos: ')
+    acao_listar = True
+    #Verifica se o caminho existe. Caso não exista, a ação de listar não ocorrerá.
+    if os.path.exists(diretorio):
+        None
+    else:
+        acao_listar = False
+    if acao_listar == True:
+        lista_diretorio = os.listdir(diretorio)
+        lista_arq = []
+        for arquivo in lista_diretorio:
+            caminho_arq = os.path.join(diretorio, arquivo)
+            if os.path.isdir(caminho_arq):
+                None
+            else:
+                lista_arq.append(arquivo)
+        return lista_arq
+    else:
+        print("O diretório fornecido não existe. Verifique e tente novamente.")
+    
+
+#Ordernar documentos (OK)
+def listar_doc_dir_org():
+    ''' Esta função lista todos os documentos eletrônicos disponíveis no diretório da biblioteca e separa pelo tipo de arquivo de forma ordenada.
+        A função "listar_dir" é chamada para fornecer a lista do diretório para a função atual.
+        A função retorna um dicionário contendo listas de diferentes formato de documento.'''
+    
     #Mensagem de inicialização de organização de arquivos
-    print(f"Listando os documentos do diretório solicitado...")
+    print(f"Iniciando o processo de ordernação...")
 
     #Criando listas separando por tipo de arquivo
     biblioteca = {
@@ -62,7 +114,7 @@ def listar_doc_dir(diretorio):
         'diverso': []
     }
     #Listando documentos e adicionando em respectivas listas
-    for arquivo in os.listdir(diretorio):
+    for arquivo in listar_dir():
         if arquivo.endswith('.pdf'):
             biblioteca['pdf'].append(arquivo)
         elif arquivo.endswith('.epub'):
@@ -91,6 +143,7 @@ def listar_doc_dir(diretorio):
             biblioteca['htm'].append(arquivo)
         else:
             biblioteca['diverso'].append(arquivo)
+
     #organizando as listas
     biblioteca['pdf'].sort()
     biblioteca['ePUB'].sort()
@@ -107,21 +160,64 @@ def listar_doc_dir(diretorio):
     biblioteca['htm'].sort()
     biblioteca['diverso'].sort()
     
-    #printando o diretório da biblioteca
+    #printando o diretório com os arquivos separados por tipo, ignorando as listas com valores de arquivo vazia.
     for chave, valor in biblioteca.items():
-        if chave !=  'diverso':
-            print(f"Os arquivos em formato {chave} são: {valor}")
-        else:
-            print(f"Os arquivos em outros tipos de formato neste diretório são: {valor}.")
+        if not valor: #Listas vazias não são printadas.
+            continue
+        elif chave !=  'diverso':
+            print(f'Os documentos no formato de {chave} são: ')   
+            for titulo in valor:
+                print(f'{titulo}')         
+        elif chave == 'diverso':
+            print(f"Os arquivos em outros tipos de formato neste diretório são: ")
+            for titulo in valor:
+                print(f'{titulo}')  
     return biblioteca
 
-#Adicionar documento no diretório
+#Adicionar documento no diretório (duas formas)
+def adicionar_doc_na_lista():
+    '''Esta função define uma das formas de adicionar arquivo à lista Biblioteca_virtual global: incluindo chave-valor via input.'''
+    #Chamando o dicionário global para dentro da função
+    global Biblioteca_virtual
+    resposta = 's'
+    #Permite adicionar mais de um título (chave-valor) no mesmo loop.
+    while resposta == 's':
+        print("Digite as informações solicitadas.")
+        titulo_doc = (input(f'Digite o título do documento: '))
+        data_publicacao = (input(f'Digite o ano de publicação do documento: '))
+        tipo_arq = (input('Digite o formato do documento (pdf/epub/doc/docx/txt) em letra minúscula: '))
+        #Adicionando o chave-valor obtido via input ao biblioteca global
+        (Biblioteca_virtual).update({
+            titulo_doc:{
+            'data_publicacao': data_publicacao,
+            'tipo': ('.'+ tipo_arq)
+        }})
+        resposta = input('Deseja inserir mais um título? (s/n): ')  #define se o loop continua
+        if resposta == 'n':
+            visualizar = input('Deseja visualizar a lista atualizada? (s/n): ') #Pergunta se o usuário quer visualizar a lista atualizada
+            if visualizar == 's':
+                for titulo, valor in sorted(Biblioteca_virtual.items()):
+                    print(titulo, valor)
+            elif visualizar == 'n':
+                None
+            else:
+                print("Resposta invalida.")   
+            print(f"A adição do(s) título(s) na lista da biblioteca foi concluída") 
+            break #Finaliza o loop
+        else:
+            print("Entrada invalida.")
+            break #finaliza loop com a entrada incorreta.
+    return Biblioteca_virtual
+    
+#def adicionar_doc_via_dir():
+    '''Esta função permite adicionar um documento à dicionário "Biblioteca_virtual" global através do diretório'''
+    
 
+        
+    
 #Renomear documento do diretório
 
 #Remover documento do diretório
-
-#Adicionar documento na lista biblioteca
 
 #Renomear documento na lista biblioteca
 
@@ -133,7 +229,9 @@ def listar_doc_dir(diretorio):
 
 #Código principal
 #Aqui encontra-se a interface de interação com o usuário
-diretorio_desejado = input(r'Digite o diretório da biblioteca em que deseja trabalhar: ')
-listar_doc_dir(diretorio_desejado)
+#diretorio_desejado = input(r'Digite o diretório da biblioteca em que deseja trabalhar: ')
+#adicionar_doc_na_lista(Biblioteca_virtual)
+
+listar_doc_dir_org()
 
 
